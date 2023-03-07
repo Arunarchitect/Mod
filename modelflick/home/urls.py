4ec101fff2dee1b0becb2ce276fc9af33ec52e10
Arun <intformbase@gmail.com>
2023-03-08 03:00:35 +0530

    Added service form. applied migrations

--- modelflick/home/urls.py
+++ modelflick/home/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('service', views.service, name='service'),
+    path('service/result/', views.result, name='result'),
 ]
--- modelflick/home/views.py
+++ modelflick/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import serviceform
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -8,4 +9,23 @@ def about(request):
     return render(request,'about.html')
 
 def service(request):
-    return render(request,'service.html')
+    form = serviceform()
+    dict_form = {
+        'form': form
+    }
+    return render(request,'service.html', dict_form)
+
+def result(request):
+    if request.method == 'POST':
+        # process form data here
+        name = request.POST.get('Name')
+        area = request.POST.get('area_of_site')
+        choice = request.POST.get('Your_dream_building')
+
+        # render results page with context
+        context = {
+            'name': name,
+            'area': area,
+            'building': choice
+        }
+        return render(request, 'result.html', context)
